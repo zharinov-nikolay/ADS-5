ea591e61fab160ccf26621a68171e0b92df10475
anton <ashtanyuk@gmail.com>
2023-01-30 19:54:23 +0300

    Initial commit

---
+++ include/tstack.h
@@ -0,0 +1,10 @@
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+
+template<typename T, int size>
+class TStack {
+  // добавьте код стека
+};
+
+#endif  // INCLUDE_TSTACK_H_
